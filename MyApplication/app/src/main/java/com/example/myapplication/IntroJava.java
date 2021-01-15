1df21e5e6a7210612ecefc566b1cc983134e3756
honnage <hon_da-1998@hotmail.com>
2021-01-16 01:40:00 +0700

    IntroJava

---
+++ MyApplication/app/src/main/java/com/example/myapplication/IntroJava.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+public class IntroJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World ");
+
+        int love = 0;
+        String msg = "love level is ";
+        System.out.println(msg + love);
+
+        love = 1;
+        System.out.println(msg + love);
+
+        love = 10;
+        System.out.println(msg + love);
+    }
+}
