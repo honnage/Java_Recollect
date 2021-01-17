0bebb4b52e40677863053a9495817389695dedd0
honnage <hon_da-1998@hotmail.com>
2021-01-18 01:47:18 +0700

    Test Function

---
+++ MyApplication/app/src/main/java/com/example/myapplication/FunctionJava.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+public class FunctionJava {
+    public static void main(String[] args) {
+        print();
+        
+        runSomtiong();
+    }
+
+    private static void runSomtiong() {
+
+    }
+
+    public static void print(){
+        System.out.println("Hey");
+        System.out.println("Hi");
+    }
+
+}
