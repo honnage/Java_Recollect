a1a02471ef7b859540161a6dc73dcaf40ef7d306
honnage <hon_da-1998@hotmail.com>
2021-01-18 01:57:13 +0700

    Test Function return

--- MyApplication/app/src/main/java/com/example/myapplication/FunctionJava.java
+++ MyApplication/app/src/main/java/com/example/myapplication/FunctionJava.java
@@ -3,9 +3,24 @@ package com.example.myapplication;
 public class FunctionJava {
     public static void main(String[] args) {
         print();
-        
+      
         runSomtiong();
+        String grade = getGrade();
+        System.out.println("Grade is "+grade);
+
+        boolean isRunning = isRunning();
+        System.out.println("It is running "+ isRunning);
     }
+
+    public static  boolean isRunning(){
+        return true;
+    }
+
+
+    public static String getGrade() {
+        return "A";
+    }
+
 
     private static void runSomtiong() {
 
