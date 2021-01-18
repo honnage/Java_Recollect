47bc7b1f70e2b05f3ed5f25a1cd7c727c7f321cd
honnage <hon_da-1998@hotmail.com>
2021-01-19 02:10:06 +0700

    Function Argument

--- MyApplication/app/src/main/java/com/example/myapplication/FunctionJava.java
+++ MyApplication/app/src/main/java/com/example/myapplication/FunctionJava.java
@@ -3,27 +3,26 @@ package com.example.myapplication;
 public class FunctionJava {
     public static void main(String[] args) {
         print();
-      
-        runSomtiong();
+
         String grade = getGrade();
         System.out.println("Grade is "+grade);
 
         boolean isRunning = isRunning();
         System.out.println("It is running "+ isRunning);
+
+        printIt("hon");
+    }
+
+    private static void printIt(String hello_hon) {
+        System.out.println("It is " + hello_hon);
     }
 
     public static  boolean isRunning(){
         return true;
     }
 
-
     public static String getGrade() {
         return "A";
-    }
-
-
-    private static void runSomtiong() {
-
     }
 
     public static void print(){
