a9571ee974b8e4f86be6bbc794c9dca1d4f89653
honnage <hon_da-1998@hotmail.com>
2021-01-20 03:56:00 +0700

    Test Error msg

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ErrorJave.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+public class ErrorJave {
+    public static void main(String[] args) {
+        // Sybcax error
+        // int out = 1
+
+        // Runtime error
+        /*
+            int a = 10;
+            int b = 0;
+            int c= a/b;
+            System.out.println("show "+c);
+        */
+
+        // Logic error
+        int a = 1;
+        int b = 2;
+
+        float c =  a/b;
+        //value logic = 0.5
+        //Value result = 0.0
+        System.out.println("result " + c);
+    }
+}
